611c73b32202e5c06f1ff620480459cd35a80854
Prasanth P <129147273+p-prasanth2609@users.noreply.github.com>
2023-07-19 11:16:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+def largest(a):
+    if sum(a)==0:
+        return '0'
+    for i in range(len(a)):
+        a[i]=str(a[i])
+    for i in range(len(a)):
+        for j in range(i+1,len(a)):
+            if a[j]+a[i]>a[i]+a[j]:
+                a[i],a[j]=a[j],a[i]
+    return ''.join(a)
 a=list(map(int,input().split()))
-for i in range(len(a)):
-    a[i]=str(a[i])
-for i in range(len(a)):
-    for j in range(i+1,len(a)):
-        if a[j]+a[i]>a[i]+a[j]:
-            a[i],a[j]=a[j],a[i]
-b=''.join(a)
-print(b)
+print(largest(a))
